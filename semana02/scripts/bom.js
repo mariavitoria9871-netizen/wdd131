458d38ebd9ae8964140a65fefc068b8a0760a374
mariavitoria9871 <mvitoria9871@byupatway.edu>
2026-07-06 22:06:56 -0300

    ok

---
+++ semana02/scripts/bom.js
@@ -0,0 +1,29 @@
+const input = document.querySelector('#capfav');
+const botao = document.querySelector('button');
+const lista = document.querySelector('ul');
+
+botao.addEventListener('click', function () {
+
+    if (input.value.trim() === '') {
+        input.focus();
+        return;
+    }
+
+    const li = document.createElement('li');
+    const botaoExcluir = document.createElement('button');
+
+    li.textContent = input.value;
+    botaoExcluir.textContent = '❌';
+
+    li.append(botaoExcluir);
+    lista.append(li);
+
+    input.value = '';
+    input.focus();
+
+    botaoExcluir.addEventListener('click', function () {
+        lista.removeChild(li);
+        input.focus();
+    });
+
+});
