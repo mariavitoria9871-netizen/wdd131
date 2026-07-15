a52bbfac26fa0883f4352ec68a4829c46800e2a3
mariavitoria9871 <mvitoria9871@byupatway.edu>
2026-07-14 21:04:45 -0300

    Tarefa: Página do País

---
+++ scripts/local.js
@@ -0,0 +1,22 @@
+const temperature = 10;
+const windSpeed = 5;
+
+function calculateWindChill(temp, speed) {
+    return (
+        13.12 +
+        0.6215 * temp -
+        11.37 * Math.pow(speed, 0.16) +
+        0.3965 * temp * Math.pow(speed, 0.16)
+    ).toFixed(1);
+}
+
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+document.getElementById("lastModified").textContent = document.lastModified;
+
+if (temperature <= 10 && windSpeed > 4.8) {
+    document.getElementById("windchill").textContent =
+        calculateWindChill(temperature, windSpeed) + " °C";
+} else {
+    document.getElementById("windchill").textContent = "N/A";
+}
